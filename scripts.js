1a796a41b590edb8d79518315495b1df98e29247
THEMIDDLEMANOFTHEYEAR <108837559+THEMIDDLEMANOFTHEYEAR@users.noreply.github.com>
2024-04-28 12:02:02 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Add animations
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(element => {
+        element.classList.add('fade-in');
+    });
+});
